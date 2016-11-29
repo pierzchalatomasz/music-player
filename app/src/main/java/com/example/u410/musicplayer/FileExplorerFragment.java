25fde54cac7ee0c6a0139440e106ea71ef507531
KrygTomasz <yaami93@gmail.com>
2016-11-29 10:29:38 +0100

    Tabbed Activity
    Implemented Tabbed Activity (with 2x File Explorer for now) instead of
    Explorer Activity.
    Little changes and fixes in code.

--- app/src/main/java/com/example/u410/musicplayer/FileExplorerFragment.java
+++ app/src/main/java/com/example/u410/musicplayer/FileExplorerFragment.java
@@ -1,14 +1,18 @@
 package com.example.u410.musicplayer;
 
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.ListView;
+
+import com.example.u410.musicplayer.ExplorerListItemAdapter;
+import com.example.u410.musicplayer.R;
+import com.example.u410.musicplayer.TabExplorerActivity;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,27 +20,45 @@ import java.util.ArrayList;
 import butterknife.ButterKnife;
 import butterknife.OnItemClick;
 
-public class ExplorerActivity extends AppCompatActivity {
+public class FileExplorerFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
     private ArrayList<String> mFiles = new ArrayList<>();
     private String mPath = Environment.getRootDirectory().toString();
-    //private ArrayAdapter<String> mAdapter;
     private ExplorerListItemAdapter mAdapter;
     private ListView mExplorerListView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explorer);
+    public FileExplorerFragment() {
+    }
 
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static FileExplorerFragment newInstance(int sectionNumber) {
+        FileExplorerFragment fragment = new FileExplorerFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_explorer_listview, container, false);
         mAdapter = new ExplorerListItemAdapter(mFiles);
-        mExplorerListView = (ListView) findViewById(R.id.explorer_list);
+        mExplorerListView = (ListView) rootView.findViewById(R.id.explorer_list);
         mExplorerListView.setAdapter(mAdapter);
 
         getFiles();
-
-        ButterKnife.bind(this);
         mAdapter.notifyDataSetChanged();
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
 
     public void getFiles() {
@@ -53,14 +75,15 @@ public class ExplorerActivity extends AppCompatActivity {
     @OnItemClick(R.id.explorer_list)
     public void onExplorerListViewItemClick(ListView parent, View view, int position, long id) {
 
-            CheckBox cb = (CheckBox) view.findViewById(R.id.checkBox);
-            cb.setChecked(!cb.isChecked());
-            mAdapter.notifyDataSetChanged();
+        CheckBox cb = (CheckBox) view.findViewById(R.id.checkBox);
+        cb.setChecked(!cb.isChecked());
+        mAdapter.notifyDataSetChanged();
 
 
         String clickedFileName = (String) mAdapter.getItem(position);
         String tempPath = createPathOfClickedFile(clickedFileName);
         tryGoToPath(tempPath);
+        mExplorerListView.setSelectionAfterHeaderView();
         //Clicking twice on directory with no permissions will crash an app (fileName will append to mPath but will not go to that path)
     }
 
@@ -90,25 +113,5 @@ public class ExplorerActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.d("Wrong directory","No permissions to this directory!");
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_explorer_save_playlist, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_save:
-                savePlaylist();
-                break;
-        }
-        
-        return true;
-    }
-
-    private void savePlaylist() {
     }
 }
--- app/src/main/java/com/example/u410/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/u410/musicplayer/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, ExplorerActivity.class);
+        Intent intent = new Intent(this, TabExplorerActivity.class);
         startActivity(intent);
     }
 }
